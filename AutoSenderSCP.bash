9b77f6dc5ebb9ae9466d267a7caea98cdb182156
Robin Valtier <robin.valtier@ecole.ensicaen.fr>
2023-05-25 13:16:46 +0200

    Add IP and User Parameters

--- AutoSenderSCP.bash
+++ AutoSenderSCP.bash
@@ -13,11 +13,13 @@
 ###########################################
 
 # CONSTANTS
-ALGORITHMS="regress/ssh-*
+ALGORITHMS="regress/ssh-*"
 
 # @brief Sends a file with SCP using all OQS-OpenSSH available algorithms
 # @param $1 The file to send
 # @param $2 The root directory of oqs-openssh (the path of the directory where there is the subdir regress )
+# @param $3 user
+# @param $4 IP Address
 function sendFileThroughAllAlgorithms()
 {
 	echo "Sending $1 :"
@@ -28,21 +30,23 @@ function sendFileThroughAllAlgorithms()
 		
 		algorithm=$(echo $privateKey | cut -d'/' -f6)
 		echo -ne " With $algorithm algorithm : ... "
-		$(~/openssh/scp -F ~/openssh/regress/ssh_config -o KexAlgorithms=bike-l1-sha512 -o HostKeyAlgorithms=$algorithm -o PubkeyAcceptedKeyTypes=$algorithm -o PasswordAuthentication=no -i ~/openssh/regress/$algorithm $1 mif73291@193.219.91.103:~/)
+		$($2scp -F $2regress/ssh_config -o KexAlgorithms=bike-l1-sha512 -o HostKeyAlgorithms=$algorithm -o PubkeyAcceptedKeyTypes=$algorithm -o PasswordAuthentication=no -i ~/openssh/regress/$algorithm $1 $3@$4:~/)
 		echo "Done"	
 		sleep 2
 	done
 }
 
 
-if [ $# -ne 2 ]
+################## MAIN ##############
+
+if [ $# -ne 4 ]
 then
 	echo "AutoSenderSCP"
 	echo " Usage :"
-	echo -e "\t$0 <filePath> <oqs-opensshPath>"
+	echo -e "\t$0 <filePath> <abspath-oqs-opensshPath> <user> <IP>"
 	echo -e " Example :"
-	echo -e "\t$0 ~/smallFile.txt ~/openssh" 
+	echo -e "\t$0 ~/smallFile.txt ~/openssh/ mif1233 10.10.10.10"
 	exit
 fi
 
-sendFileThroughAllAlgorithms $1 $2
+sendFileThroughAllAlgorithms $1 $2 $3 $4
